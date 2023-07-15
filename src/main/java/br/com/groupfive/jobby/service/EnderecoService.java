c7860670897976005c4bdc929fbb0b116d4d195c
EndryoMachado <endryocostamachado@gmail.com>
2023-07-15 16:42:52 -0300

    @EndryoMachado implementando métodos de contrato da classe EnderecoService

--- src/main/java/br/com/groupfive/jobby/service/EnderecoService.java
+++ src/main/java/br/com/groupfive/jobby/service/EnderecoService.java
@@ -1,31 +1,64 @@
 package br.com.groupfive.jobby.service;
 
+import br.com.groupfive.jobby.dto.cidade.CidadeDTO;
 import br.com.groupfive.jobby.dto.endereco.CreateEnderecoDTO;
 import br.com.groupfive.jobby.dto.endereco.EnderecoDTO;
 import br.com.groupfive.jobby.dto.endereco.UpdateEnderecoDTO;
+import br.com.groupfive.jobby.model.Cidade;
+import br.com.groupfive.jobby.model.Endereco;
+import br.com.groupfive.jobby.repository.CidadeRepository;
+import br.com.groupfive.jobby.repository.EnderecoRepository;
 import br.com.groupfive.jobby.service.interfaces.IEnderecoService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EnderecoService implements IEnderecoService<Integer> {
 
+    @Autowired
+    EnderecoRepository enderecoRepository;
+    @Autowired
+    CidadeRepository cidadeRepository;
     @Override
     public EnderecoDTO findById(Integer id) {
-        return null;
+        if(enderecoRepository.existsById(id)){
+            Endereco enderecoModel = enderecoRepository.findById(id).get();
+            return EnderecoDTO.fromModel(enderecoModel);
+        }else{
+            return null;
+        }
     }
 
     @Override
     public EnderecoDTO create(CreateEnderecoDTO createEnderecoDTO) {
-        return null;
+        Endereco enderecoModel = new Endereco();
+        BeanUtils.copyProperties(createEnderecoDTO, enderecoModel);
+        enderecoModel.setCidade(cidadeRepository.findById(createEnderecoDTO.idCidade()).get());
+        enderecoRepository.save(enderecoModel);
+        return EnderecoDTO.fromModel(enderecoModel);
     }
 
     @Override
     public boolean update(UpdateEnderecoDTO updateEnderecoDTO, Integer id) {
-        return false;
+        if(enderecoRepository.existsById(id)){
+            Endereco enderecoModel = enderecoRepository.findById(id).get();
+            BeanUtils.copyProperties(updateEnderecoDTO, enderecoModel);
+            enderecoModel.setCidade(cidadeRepository.findById(updateEnderecoDTO.idCidade()).get());
+            enderecoRepository.save(enderecoModel);
+            return true;
+        }else{
+            return false;
+        }
     }
 
     @Override
     public boolean deleteById(Integer id) {
-        return false;
+        if(enderecoRepository.existsById(id)){
+            enderecoRepository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
     }
 }
