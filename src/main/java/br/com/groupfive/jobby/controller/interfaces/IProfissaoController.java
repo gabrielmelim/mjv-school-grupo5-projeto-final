cfea94ea91bea2924ce4a06b0489397520133acc
FernandoVictorff <fernandovictor01@gmail.com>
2023-07-17 17:55:38 -0300

    Acrescentando findAll de Profissao

--- src/main/java/br/com/groupfive/jobby/controller/interfaces/IProfissaoController.java
+++ src/main/java/br/com/groupfive/jobby/controller/interfaces/IProfissaoController.java
@@ -4,8 +4,11 @@ import br.com.groupfive.jobby.dto.profissao.CreateProfissaoDTO;
 import br.com.groupfive.jobby.dto.profissao.ProfissaoDTO;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface IProfissaoController<T> {
     ResponseEntity<ProfissaoDTO> findById(T id);
+    ResponseEntity<List<ProfissaoDTO>> findAll();
     ResponseEntity<ProfissaoDTO> create(CreateProfissaoDTO createProfissaoDTO);
     ResponseEntity deleteById(T id);
 }
--- src/main/java/br/com/groupfive/jobby/service/ProfissaoService.java
+++ src/main/java/br/com/groupfive/jobby/service/ProfissaoService.java
@@ -1,13 +1,18 @@
 package br.com.groupfive.jobby.service;
 
+import br.com.groupfive.jobby.dto.cidade.CidadeDTO;
 import br.com.groupfive.jobby.dto.profissao.CreateProfissaoDTO;
 import br.com.groupfive.jobby.dto.profissao.ProfissaoDTO;
+import br.com.groupfive.jobby.model.Cidade;
 import br.com.groupfive.jobby.model.Profissao;
 import br.com.groupfive.jobby.repository.ProfissaoRepository;
 import br.com.groupfive.jobby.service.interfaces.IProfissaoService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProfissaoService implements IProfissaoService<Integer> {
@@ -41,5 +46,11 @@ public class ProfissaoService implements IProfissaoService<Integer> {
         }else{
             return false;
         }
+    }
+
+    @Override
+    public List<ProfissaoDTO> findAll() {
+        List<Profissao> profissoes = profissaoRepository.findAll();
+        return profissoes.isEmpty() ? null : profissoes.stream().map(p -> ProfissaoDTO.fromModel(p)).collect(Collectors.toList());
     }
 }
