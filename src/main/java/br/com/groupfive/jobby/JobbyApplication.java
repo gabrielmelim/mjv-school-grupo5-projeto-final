92bd008bb472bdf35426de8045af27616d328aeb
FernandoVictorff <fernandovictor01@gmail.com>
2023-07-16 21:41:35 -0300

    @FernandoVictorff Refatorando para funcionamento swagger

--- src/main/java/br/com/groupfive/jobby/JobbyApplication.java
+++ src/main/java/br/com/groupfive/jobby/JobbyApplication.java
@@ -1,7 +1,12 @@
 package br.com.groupfive.jobby;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JobbyApplication {
